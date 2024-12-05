ae787edea0b0c0f78c7619350d43d85c7fbbda40
Hamdika <hamdika.hidayat.m@gmail.com>
2024-12-05 21:54:41 +0700

    clean

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,15 +1,18 @@
-import { Route, Redirect, RouteProps } from 'react-router-dom';
-import { getCurrentUser } from '../utils/auth';
+import { Route, Redirect, RouteProps } from "react-router-dom";
+import { getCurrentUser } from "../utils/auth";
 
 interface AuthGuardProps extends RouteProps {
   component: React.ComponentType<any>;
 }
 
-const AuthGuard: React.FC<AuthGuardProps> = ({ component: Component, ...rest }) => {
+const AuthGuard: React.FC<AuthGuardProps> = ({
+  component: Component,
+  ...rest
+}) => {
   return (
     <Route
       {...rest}
-      render={props => {
+      render={(props) => {
         const currentUser = getCurrentUser();
         if (!currentUser) {
           return <Redirect to="/login" />;
